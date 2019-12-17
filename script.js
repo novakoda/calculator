a12e369e0929b71fe1d08faa2b1253f42d82855f
novakoda <novakoda9@gmail.com>
2019-12-17 12:36:36 -0600

    Add basic functionality

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ function operate(num1, op, num2) {
     case '-':
       answer = subtract(num1, num2);
       break;
+    case '×':
+      answer = multiply(num1, num2);
+      break;
     case '*':
       answer = multiply(num1, num2);
       break;
@@ -33,22 +36,60 @@ function operate(num1, op, num2) {
   return answer;
 };
 
-function updateDisplay(number) {
+function numberClicked(number) {
+  if (displayNumber == 0 || answered === true) {
+    displayNumber = number.toString();
+    currentDisplay.innerHTML = displayNumber;
+    if (answered === true) {answered = false;};
+  } else {
   displayNumber = displayNumber.toString() + number.toString();
-  resultDisplay.innerHTML = displayNumber;
+  currentDisplay.innerHTML = displayNumber;
+  };
+};
+
+function storeNumber(number, op) {
+  operator = op.toString();
+  previousNumber = displayNumber.toString() + ' ' + operator;
+  displayNumber = '0';
+  historyDisplay.innerHTML = previousNumber;
+  currentDisplay.innerHTML = displayNumber;
+};
+
+function equalize() {
+  num1 = Number(previousNumber.slice(0,-2));
+  num2 = Number(displayNumber);
+  displayNumber = operate(num1, operator, num2).toString();
+  previousNumber = '0';
+  historyDisplay.innerHTML = '';
+  currentDisplay.innerHTML = displayNumber;
+  answered = true;
 };
 
 let displayNumber = '0';
+let previousNumber = '0';
+let operator = '';
+let answered = false;
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
-const equalButton = document.querySelectorAll('[data-equal]');
-const backButton = document.querySelectorAll('[data-back]');
-const clearButton = document.querySelectorAll('[data-clear]');
-const resultDisplay = document.getElementById('result');
+const equalButton = document.querySelector('[data-equal]');
+const backButton = document.querySelector('[data-back]');
+const clearButton = document.querySelector('[data-clear]');
+const currentDisplay = document.getElementById('result');
+const historyDisplay = document.getElementById('history');
 
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
-    updateDisplay(button.innerHTML);
+    numberClicked(button.innerHTML);
   });
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    storeNumber(displayNumber, button.innerHTML);
+  });
+});
+
+equalButton.addEventListener('click', () => {
+  equalize();
 });
